1f75037f0d2c02e3e3bd4414c95a86bc2c43208b
Hipfel, Stefan <>
2019-11-04 15:28:55 +0100

    updates storage interface

--- pkg/storage/interface.go
+++ pkg/storage/interface.go
@@ -7,10 +7,10 @@ import (
 
 type Storage interface {
 	WriteBytes(f string, b []byte) (err error)
-	WriteFile(path string) (err error)
 	WriteFolder(p string) (err error)
 	WriteStream(name, mimeType string, body io.Reader) (err error)
-	WriteStreamConcurrent(name, mimeType string, body io.Reader, errc chan<- *error)
 	GetBackupByTimestamp(t time.Time) (path string, err error)
-	GetLatestBackup() (path string, err error)
+	DownloadLatestBackup() (path string, err error)
+	GetAllBackups() (bl []Backup, err error)
+	DownloadBackupFrom(fullBackupPath string, binlog string) (path string, err error)
 }
