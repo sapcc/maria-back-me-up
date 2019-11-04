f74d0e914b7f91318d7a3e1ce07267e5e92e8ab3
Hipfel, Stefan <>
2019-11-04 15:37:45 +0100

    makes use of a port constant in the server pkg

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,10 +2,10 @@ package server
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sapcc/maria-back-me-up/pkg/constants"
 	"github.com/sapcc/maria-back-me-up/pkg/log"
 )
 
@@ -21,7 +21,7 @@ func NewServer(e *echo.Echo) *Server {
 }
 
 func (s *Server) Start() (err error) {
-	if err = s.echo.Start(":8081"); err != nil {
+	if err = s.echo.Start(":" + constants.PORT); err != nil {
 		log.Error("shutting down the server")
 		return
 	}
@@ -32,12 +32,4 @@ func (s *Server) Stop(ctx context.Context) {
 	if err := s.echo.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func (s *Server) health(w http.ResponseWriter, r *http.Request) {
-	if true {
-		w.WriteHeader(http.StatusOK)
-		return
-	}
-	w.WriteHeader(http.StatusServiceUnavailable)
 }
