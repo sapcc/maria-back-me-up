b04374ee6eb8ec78026a47b58e9ed8f1312bf235
Hipfel, Stefan <>
2020-03-05 10:28:13 +0100

    adds logging to config file loading

--- cmd/verification/main.go
+++ cmd/verification/main.go
@@ -35,7 +35,7 @@ func init() {
 func main() {
 	cfg, err := config.GetConfig(opts)
 	if err != nil {
-		log.Fatal("cannot load config file")
+		log.Fatal("cannot load config file", err.Error())
 	}
 
 	ctx := context.Background()
