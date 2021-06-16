f4931c68ef790b91b60dfa268800504379a25c90
Ivo Gosemann <ivo.gosemann@sap.com>
2021-06-16 11:20:20 +0200

    Fixes script to continue on edb check error

--- testing/with-mariadb.sh
+++ testing/with-mariadb.sh
@@ -90,7 +90,7 @@ echo "Inserting dummy data into tasks table finished"
 echo "Starting background check to restart MariaDB if shutdown"
 while :
   do
-    mysqladmin -h localhost -uroot -ptest ping > /dev/null
+    mysqladmin -h localhost -uroot -ptest ping > /dev/null || continue
     if [ $? -eq 0 ]
       then
           echo "mariadb is up"
