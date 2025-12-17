64434e23c86bb18f7982ace662a9a5c7c726ab4a
Vladislav Gusev <vladislav.gusev@sap.com>
2025-12-17 12:29:29 +0200

    Add license headers

--- pkg/test/testclean.sql
+++ pkg/test/testclean.sql
@@ -1,2 +1,6 @@
+-- SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company
+--
+-- SPDX-License-Identifier: Apache-2.0
+
 DROP DATABASE IF EXISTS service;
 DROP DATABASE IF EXISTS application;
--- static/js/ui.js
+++ static/js/ui.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company
+//
+// SPDX-License-Identifier: Apache-2.0
+
 let enableButton = function(selector, btnText) {
     const btn = document.getElementById(selector)
     btn.disabled = false
